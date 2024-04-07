d1f05ec8c7f5fd91c3bdfcccee02ff79fa54a5ca
RomanSavitskiy <roma.savitskiy@inbox.ru>
2024-04-07 16:40:06 +0300

    fix yaml configuration

--- src/main/java/com/example/healthassistant/repository/UserRepository.java
+++ src/main/java/com/example/healthassistant/repository/UserRepository.java
@@ -4,6 +4,4 @@ import com.example.healthassistant.model.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-
-
 }
