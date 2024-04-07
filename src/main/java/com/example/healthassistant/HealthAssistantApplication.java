cf9e9814814443837f266370bb243d4f83904c68
RomanSavitskiy <roma.savitskiy@inbox.ru>
2024-04-07 17:15:28 +0300

    fix swagger configuration for testing

--- src/main/java/com/example/healthassistant/HealthAssistantApplication.java
+++ src/main/java/com/example/healthassistant/HealthAssistantApplication.java
@@ -1,10 +1,13 @@
 package com.example.healthassistant;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Profile;
 
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "Default Server URL")})
 public class HealthAssistantApplication {
     @Profile({"dev"})
 
