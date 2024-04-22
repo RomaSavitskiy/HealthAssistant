7b685297af85ec23244be58fa51528eaa99ab0be
RomanSavitskiy <roma.savitskiy@inbox.ru>
2024-04-22 14:33:14 +0300

    Remove id from AdviceResponseTo

--- src/main/java/com/example/healthassistant/model/response/AdviceResponseTo.java
+++ src/main/java/com/example/healthassistant/model/response/AdviceResponseTo.java
@@ -3,7 +3,6 @@ package com.example.healthassistant.model.response;
 import com.example.healthassistant.model.enums.AdviceCategory;
 
 public record AdviceResponseTo(
-        Long id,
         String title,
         String text,
         AdviceCategory category
